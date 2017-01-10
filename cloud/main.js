f3319e82ddc93d524bada8022fe99b5cf28fbf14
nickprak23 <nickprak23@gmail.com>
2017-01-09 23:14:41 -0500

    cloud code updated with further functions

--- cloud/main.js
+++ cloud/main.js
@@ -44,8 +44,108 @@ Parse.Cloud.define('dropInRewardsRedeemedToday', function(req, res) {
       for (var i = 0; i < results.length; i++) {
         var object = results[i];
 
-        var rewardsEarnedToday = object.get('rewardsRedeemed');
-        res.success(rewardsEarnedToday);
+        res.success(object.get('rewardsRedeemed'));
+      }
+
+    },
+    error: function(error) {
+      res.error("Error: " + error.code + " " + error.message);
+    }
+  });
+
+});
+
+
+// drop in user signups today
+
+Parse.Cloud.define('dropInUserSignupsToday', function(req, res) {
+
+  var BarMetrics = Parse.Object.extend("Metrics_DropIn");
+  var query = new Parse.Query(BarMetrics);
+  query.descending("date");
+  query.limit(1);
+  query.find({
+    success: function(results) {
+      for (var i = 0; i < results.length; i++) {
+        var object = results[i];
+
+        res.success(object.get('userSignups'));
+      }
+
+    },
+    error: function(error) {
+      res.error("Error: " + error.code + " " + error.message);
+    }
+  });
+
+});
+
+
+// drop in total users since v2
+
+Parse.Cloud.define('dropInTotalUsers', function(req, res) {
+
+  var BarMetrics = Parse.Object.extend("Metrics_DropIn");
+  var query = new Parse.Query(BarMetrics);
+  query.descending("date");
+  query.limit(1);
+  query.find({
+    success: function(results) {
+      for (var i = 0; i < results.length; i++) {
+        var object = results[i];
+
+        res.success(object.get('totalUsers'));
+      }
+
+    },
+    error: function(error) {
+      res.error("Error: " + error.code + " " + error.message);
+    }
+  });
+
+});
+
+
+// drop in total male users since v2
+
+Parse.Cloud.define('dropInTotalMaleUsers', function(req, res) {
+
+  var BarMetrics = Parse.Object.extend("Metrics_DropIn");
+  var query = new Parse.Query(BarMetrics);
+  query.descending("date");
+  query.limit(1);
+  query.find({
+    success: function(results) {
+      for (var i = 0; i < results.length; i++) {
+        var object = results[i];
+
+        res.success(object.get('totalUsersMale'));
+      }
+
+    },
+    error: function(error) {
+      res.error("Error: " + error.code + " " + error.message);
+    }
+  });
+
+});
+
+
+
+// drop in total female users since v2
+
+Parse.Cloud.define('dropInTotalFemaleUsers', function(req, res) {
+
+  var BarMetrics = Parse.Object.extend("Metrics_DropIn");
+  var query = new Parse.Query(BarMetrics);
+  query.descending("date");
+  query.limit(1);
+  query.find({
+    success: function(results) {
+      for (var i = 0; i < results.length; i++) {
+        var object = results[i];
+
+        res.success(object.get('totalUsersFemale'));
       }
 
     },
@@ -58,4 +158,3 @@ Parse.Cloud.define('dropInRewardsRedeemedToday', function(req, res) {
 
 
 ///########### cloud code function for Drop In Insight ##############################
-
