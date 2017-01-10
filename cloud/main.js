03f564f66085a7af167948ea7bae76d3cd51f44b
nickprak23 <nickprak23@gmail.com>
2017-01-09 23:02:02 -0500

    cloud code updated:wq

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,61 @@
 
 Parse.Cloud.define('hello', function(req, res) {
-  res.success('Hi');
+  res.success('Hi its nick');
 });
+
+///########### cloud code function for Drop In Insight ##############################
+
+
+// dropIn rewards earned today
+
+Parse.Cloud.define('dropInRewardsEarnedToday', function(req, res) {
+
+  var BarMetrics = Parse.Object.extend("Metrics_DropIn");
+  var query = new Parse.Query(BarMetrics);
+  query.descending("date");
+  query.limit(1);
+  query.find({
+    success: function(results) {
+      for (var i = 0; i < results.length; i++) {
+        var object = results[i];
+
+        var rewardsEarnedToday = object.get('rewardsEarned');
+        res.success(rewardsEarnedToday);
+      }
+
+    },
+    error: function(error) {
+      res.error("Error: " + error.code + " " + error.message);
+    }
+  });
+
+});
+
+// drop in rewards redeemed today
+
+Parse.Cloud.define('dropInRewardsRedeemedToday', function(req, res) {
+
+  var BarMetrics = Parse.Object.extend("Metrics_DropIn");
+  var query = new Parse.Query(BarMetrics);
+  query.descending("date");
+  query.limit(1);
+  query.find({
+    success: function(results) {
+      for (var i = 0; i < results.length; i++) {
+        var object = results[i];
+
+        var rewardsEarnedToday = object.get('rewardsRedeemed');
+        res.success(rewardsEarnedToday);
+      }
+
+    },
+    error: function(error) {
+      res.error("Error: " + error.code + " " + error.message);
+    }
+  });
+
+});
+
+
+///########### cloud code function for Drop In Insight ##############################
+
